231bc4c232d63ad0d35d04897c693845bfd75e17
masakazutakewaka <takewakamma@gmail.com>
2020-05-03 22:33:50 +0900

    training - hello world
    nasm -fmacho64 hello.asm && ld -macosx_version_min 10.7.0 hello.o && ./a.out

---
+++ src/training/hello.asm
@@ -0,0 +1,21 @@
+; https://cs.lmu.edu/~ray/notes/nasmtutorial/
+; ax ... Accumlator Register. Used for MUL, DIV, etc.
+; dx ... Date Register. Used for MUL, DIV, etc.
+; si ... Source Index. Used for source address
+; di ... Destination Index. Used for destination address
+
+global start
+
+section .text
+start: mov rax, 0x02000004        ; system call for write
+       mov rdi, 1                 ; file handle 1 is stdout
+			 mov rsi, message           ; address of string to output
+			 mov rdx, 13                ; number of bytes
+			 syscall                    ; invoke operating system to do the write
+			 mov rax, 0x02000001        ; system call for exit
+			 xor rdi, rdi               ; exit code 0
+			 syscall                    ; invoke operating system to exit
+
+section .data
+message: db "Hello World", 10
+
