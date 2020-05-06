06ad43c08422ef25ab23de4041332a85a5023ed0
masakazutakewaka <takewakamma@gmail.com>
2020-05-07 01:08:53 +0900

    14.4 macro for call func

---
+++ src/include/macro.s
@@ -0,0 +1,13 @@
+%macro cdecl 1-*.no-list
+  %rep %0 -1 
+	  push %{-1:-1}      ; last arg
+		%rotate -1         ; rotate to right ->
+	%endrep
+	%rotate -1           ; return args to as it used be
+
+	call %1
+
+	%if 1 < %0
+		add sp, (__BITS__ >> 3) * (%0 - 1)     ;dump stqck (16(2byte) or 32(4byte)) * (number of args)
+	%endif
+%endmacro
